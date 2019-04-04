9cb6f44e8edbc28c8c177d0b5506be3d4b49cd8b
CAMHub PublishBot <octravis@us.ibm.com>
2019-04-04 20:49:58 +0000

    Automated CAMHub::publish on 2019-04-04 20:49:57 UTC

--- vmware/terraform/output.tf
+++ vmware/terraform/output.tf
@@ -15,3 +15,7 @@ output "ibm_cloud_private_admin_password" {
 output "ibm_cloud_private_master_ip" {
   value = "${var.cluster_vip}"
 }
+
+output "ibm_cloud_private_boot_ip" {
+  value = "${element(values(var.boot_hostname_ip),0)}"
+}
